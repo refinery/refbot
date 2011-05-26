71d359dc0966e9257082de678b63941888d3af6c
Philip Arndt <parndt@gmail.com>
2011-05-27 11:54:58 +1200

    I'm after a channel_message instead, it seems.

--- lib/responders/cookie.rb
+++ lib/responders/cookie.rb
@@ -1,8 +1,15 @@
 class Responders::Cookie< Responders::Base
   def action_message msg, who, full_name
     case msg
-      when /^gives the bot a cookie$/, /^gives the bot a snack$/, /\!botsnack$/
-        say_to_chan "I'd rather have a contribution to Refinery"
+      when /^gives the bot a cookie$/, /^gives the bot a snack$/
+        say_to_chan "#{who}: Nom! But, I'd rather have a contribution to Refinery"
+    end
+  end
+
+  def channel_message msg, who, full_name
+    case msg
+      when "!botsnack"
+        say_to_chan "#{who}: Nom! But, I'd rather have a contribution to Refinery"
     end
   end
 end
