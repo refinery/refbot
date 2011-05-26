b9e28137d2624b9ee968a05b521a60e0550747c8
Philip Arndt <parndt@gmail.com>
2011-05-27 11:45:20 +1200

    See if we can support !botsnack.

--- lib/responders/cookie.rb
+++ lib/responders/cookie.rb
@@ -1,7 +1,7 @@
 class Responders::Cookie< Responders::Base
   def action_message msg, who, full_name
     case msg
-      when /^gives the bot a cookie$/, /^gives the bot a snack$/
+      when /^gives the bot a cookie$/, /^gives the bot a snack$/, /^\!botsnack$/
         say_to_chan "I'd rather have a contribution to Refinery"
     end
   end
