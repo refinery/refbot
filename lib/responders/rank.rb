71aedbd639d8ec479e01449477fa65672f7d4c09
Philip Arndt <parndt@gmail.com>
2012-02-29 17:17:39 +1300

    Restriction seems relatively arbitrary.

--- lib/responders/rank.rb
+++ lib/responders/rank.rb
@@ -2,17 +2,17 @@ class Responders::Rank< Responders::Base
   def channel_message msg, who, full_name
     case msg
       when /^!rank (\d+)$/
-        if $1.to_i > 10
-          say_to_chan "Cannot list more than top 10 contributors"
+        if $1.to_i > 25
+          say_to_chan "Cannot list more than top 25 contributors"
           return
         end
-        
-        contributors = HTTParty.get("http://github.com/api/v2/json/repos/show/resolve/refinerycms/contributors")["contributors"]    
+
+        contributors = HTTParty.get("http://github.com/api/v2/json/repos/show/resolve/refinerycms/contributors")["contributors"]
         say_to_chan "Top #{$1} Contributors: " + contributor_list(contributors[0, $1.to_i]) if $1.to_i > 0
       when /^!rank (.+)$/
-        contributors = HTTParty.get("http://github.com/api/v2/json/repos/show/resolve/refinerycms/contributors")["contributors"]    
+        contributors = HTTParty.get("http://github.com/api/v2/json/repos/show/resolve/refinerycms/contributors")["contributors"]
         usernames = $1.gsub(", ", " ").split
-        
+
         contributors.delete_if {|c| !usernames.include?(c["login"])}
         if contributors.size > 0
           say_to_chan "Comparing Contributors: " + contributor_list(contributors)
@@ -21,15 +21,15 @@ class Responders::Rank< Responders::Base
         end
     end
   end
-  
+
   def contributor_list contributors
     contributors.sort_by!{|c| c["contributions"]}.reverse!
-    
+
     string = []
     contributors.each do |c|
       string << c["login"] + " " + c["contributions"].to_s
     end
-    
+
     string.join(", ")
   end
 end
